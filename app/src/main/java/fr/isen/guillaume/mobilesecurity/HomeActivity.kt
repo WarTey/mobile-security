77466d75a8f3a0b7926a5cb539500970723fd89b
Guillaume Blanc de Lanaute <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2020-03-02 22:46:40 +0100

    Ajout des animations dans la page Home

--- app/src/main/java/fr/isen/guillaume/mobilesecurity/HomeActivity.kt
+++ app/src/main/java/fr/isen/guillaume/mobilesecurity/HomeActivity.kt
@@ -3,6 +3,7 @@ package fr.isen.guillaume.mobilesecurity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
@@ -11,6 +12,18 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        animCard()
+
         materialCardPatients.setOnClickListener { startActivity(Intent(this, PatientsActivity::class.java)) }
+    }
+
+    private fun animCard() {
+        materialCardPatients.startAnimation(AnimationUtils.loadAnimation(this, R.anim.translation_y_one))
+        materialOldPatients.startAnimation(AnimationUtils.loadAnimation(this, R.anim.translation_y_one))
+        materialCardVisits.startAnimation(AnimationUtils.loadAnimation(this, R.anim.translation_y_two))
+        materialCardMail.startAnimation(AnimationUtils.loadAnimation(this, R.anim.translation_y_two))
+        materialCardTokens.startAnimation(AnimationUtils.loadAnimation(this, R.anim.translation_y_three))
+        materialCardSettings.startAnimation(AnimationUtils.loadAnimation(this, R.anim.translation_y_three))
+        btnLogout.startAnimation(AnimationUtils.loadAnimation(this, R.anim.translation_y_four))
     }
 }
