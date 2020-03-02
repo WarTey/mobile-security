b3ada37149e6ab4b244daac942a0e6635b7bb740
Guillaume Blanc de Lanaute <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2020-03-02 22:37:25 +0100

    Ajout de AddPatientActivity

---
+++ app/src/main/java/fr/isen/guillaume/mobilesecurity/AddPatientActivity.kt
@@ -0,0 +1,12 @@
+package fr.isen.guillaume.mobilesecurity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddPatientActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_patient)
+    }
+}
