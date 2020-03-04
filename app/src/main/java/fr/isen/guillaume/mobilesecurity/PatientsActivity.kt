586b98c24d788bd4ea2b5e0d87a60cdaa3417fec
Elio <eliob83@gmail.com>
2020-03-04 19:54:27 +0100

    - Multi Dex activé (nécessaire depuis l'ajout de Firestore) - Couleurs : Changement de la couleur d'accentuation (pour les loadingCircles) - ActivityVisits : ajout d'un loadingCircle. Récupération des visites selon le mode, affichage antichronologique. - Adapter pour les recycler view Patients et Visits - FormattedTime : correction de l'heure - Visit : ajout de l'ID -

--- app/src/main/java/fr/isen/guillaume/mobilesecurity/PatientsActivity.kt
+++ app/src/main/java/fr/isen/guillaume/mobilesecurity/PatientsActivity.kt
@@ -1,8 +1,11 @@
 package fr.isen.guillaume.mobilesecurity
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import fr.isen.guillaume.mobilesecurity.model.Patient
+import fr.isen.guillaume.mobilesecurity.recycler.PatientsAdapter
 import kotlinx.android.synthetic.main.activity_patients.*
 
 class PatientsActivity : AppCompatActivity() {
@@ -11,6 +14,19 @@ class PatientsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_patients)
 
-        floatingButtonPatients.setOnClickListener { startActivity(Intent(this, AddPatientActivity::class.java)) }
+        val patients = ArrayList<Patient>()
+        patients.add(Patient("Jean", "Dupont", null))
+
+        recyclerPatients.layoutManager = LinearLayoutManager(this)
+        recyclerPatients.adapter = PatientsAdapter(patients, this)
+
+        floatingButtonPatients.setOnClickListener {
+            startActivity(
+                Intent(
+                    this,
+                    AddPatientActivity::class.java
+                )
+            )
+        }
     }
 }
--- app/src/main/java/fr/isen/guillaume/mobilesecurity/misc/FormattedTime.kt
+++ app/src/main/java/fr/isen/guillaume/mobilesecurity/misc/FormattedTime.kt
@@ -1,12 +1,13 @@
 package fr.isen.guillaume.mobilesecurity.misc
 
+import android.util.Log
 import java.text.SimpleDateFormat
 import java.util.*
 
 class FormattedTime {
     companion object {
         fun hoursMinutes(millis: Long?): String {
-            return SimpleDateFormat("yyyy", Locale.FRENCH).format(millis)
+            return SimpleDateFormat("HH:mm", Locale.FRENCH).format(millis)
         }
 
         fun dayMonth(millis: Long?): String {
