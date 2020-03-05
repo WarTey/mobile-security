a041a6f8bb9984b86c60bd7dc424288d1cac80a9
Elio <eliob83@gmail.com>
2020-03-05 02:01:07 +0100

    - Ajout d'une activité Patient : loading, recycler et infos patient

---
+++ app/src/main/java/fr/isen/guillaume/mobilesecurity/PatientActivity.kt
@@ -0,0 +1,11 @@
+package fr.isen.guillaume.mobilesecurity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PatientActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
